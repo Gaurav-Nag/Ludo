d9fdb1b678049c4fe6a996ae5ba7525d007468fa
Gaurav-Nag <gauravnag24@outlook.com>
2018-11-03 17:34:24 +0530

    latest code

--- ludo.cpp
+++ ludo.cpp
@@ -4,89 +4,123 @@
 #include<iostream>
 
 using namespace std;
-//comment --no-all is working or not
+
+//----------------------------------------lIST OF TURNING POINTS POSITIONS TO SHOW DIRECTIONS FOR THE GOTI------------------------------------------------------ 
 int from[24][2] = {
-52, 252,
-212, 252,
-252, 212,
-252, 12,
-292, 12,
-332, 12,
-332, 52,
-332, 212,
-372, 252,
-572, 252,
-572, 292,
-572, 332,
-532, 332,
-372, 332,
-332, 372,
-332, 572,
-292, 572,
-252, 572,
-252, 532,
-252, 372,
-212, 332,
-12, 332,
-12, 292,
-12, 252
-}, 
-
-to[24][2] = {
-212, 252,
-252, 212,
-252, 12,
-292, 12,
-332, 12,
-332, 52,
-332, 212,
-372, 252,
-572, 252,
-572, 292,
-572, 332,
-532, 332,
-372, 332,
-332, 372,
-332, 572,
-292, 572,
-252, 572,
-252, 532,
-252, 372,
-212, 332,
-12, 332,
-12, 292,
-12, 252,
-52, 252
+60, 260, //52, 252,
+220, 260, //212, 252,
+260, 220, //252, 212,
+260, 20, //252, 12,
+300, 20, //292, 12,
+340, 20, //332, 12,
+340, 60, //332, 52,
+340, 220, //332, 212,
+380, 260, //372, 252,
+580, 260, //572, 252,
+580, 300, //572, 292,
+580, 340, //572, 332,
+540, 340, //532, 332,
+380, 340, //372, 332,
+340, 380, //332, 372,
+340, 580, //332, 572,
+300, 580, //292, 572,
+260, 580, //252, 572,
+260, 540, //252, 532,
+260, 380, //252, 372,
+220, 340, //212, 332,
+20, 340,  //12, 332,
+20, 300, //12, 292,
+20, 260 //12, 252
 };
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//Index for starting points of goti
+int starting[] = {0, 6, 12, 18};
+
+//safe points co-ordinates
+int safe[] = {100, 340, 260, 100, 500, 260, 340, 500};
+
+//color codes
+int color[] = {4, 2, 14, 1};
+
+//class for looks and visuals of ludo
 class chopad{
 	public:
 		void show(){
 			rectangle(0,0,600, 600);
-			for(int i = 0,j = 40;j <= 240; i = i + 40, j = j + 40){
-				for(int k = 240, a = 1; a <= 3;a++, k = k+40){
-					rectangle(k, i, k + 40, j);
-				}
+			rectangle(240,0,360, 600);
+			rectangle(0,240,600,360);
+			
+			rectangle(280, 0, 320, 240);
+			setfillstyle(SOLID_FILL, 2);
+			floodfill(300, 50, 0);
+			
+			rectangle(280, 360, 320, 600);
+			setfillstyle(SOLID_FILL, 1);
+			floodfill(300, 400, 0);
+			
+			rectangle(0, 280, 240, 320);
+			setfillstyle(SOLID_FILL, 4);
+			floodfill(50, 300, 0);
+			
+			rectangle(360, 280, 600, 320);
+			setfillstyle(SOLID_FILL, 14);
+			floodfill(400, 300, 0);
+			
+			for(int i = 0;i <= 560;i = i+80){
+				if(i >= 240 && i <= 360)
+					continue;
+				rectangle(i, 240, i + 40, 360);
 			}
-		
-			for(int i = 360,j = 400;j <= 600; i = i + 40, j = j + 40){
-				for(int k = 240, a = 1; a <= 3;a++, k = k+40){
-					rectangle(k, i, k + 40, j);
-				}
+			
+			for(int i = 0;i <= 560;i = i+80){
+				if(i >= 240 && i <= 360)
+					continue;
+				rectangle(240, i, 360, i + 40);
 			}
-			for(int i = 240,j = 280;j <= 360; i = i + 40, j = j + 40){
-				for(int k = 0, a = 1; a <= 6;a++, k = k+40){
-					rectangle(k, i, k + 40, j);
-				}
+			
+			for(int i = 0;i < 8;i = i+2){
+				setfillstyle(1, 13);
+				floodfill(safe[i], safe[i+1], 0);
 			}
-			for(int i = 240,j = 280;j <= 360; i = i + 40, j = j + 40){
-				for(int k = 360, a = 1; a <= 6;a++, k = k+40){
-					rectangle(k, i, k + 40, j);
-				}
+			
+			for(int i = 0;i < 4;i++){
+				setfillstyle(1, color[i]);
+				floodfill(from[starting[i]][0], from[starting[i]][1], 0);
 			}
+			
+			setfillstyle(1, color[0]);
+			floodfill(from[starting[0]][0], from[starting[0]][1]-40, 0);
+			rectangle(from[starting[0]][0]-20, 40, from[starting[0]][0]+140, 200);
+			circle(80, 80, 25);circle(80, 160, 25);circle(160, 80, 25);circle(160, 160, 25);
+			setfillstyle(1, 15);
+			floodfill(from[starting[0]][0], from[starting[0]][1]-80, 0);
+			
+			setfillstyle(1, color[1]);			
+			floodfill(from[starting[1]][0]+40, from[starting[1]][1]+ 40, 0);
+			rectangle(from[starting[1]][0]+60, 40, from[starting[1]][0]+220, 200);
+			circle(440, 80, 25);circle(520, 80, 25);circle(440, 160, 25);circle(520, 160, 25);
+			setfillstyle(1, 15);
+			floodfill(from[starting[1]][0]+80, from[starting[1]][1]+ 80, 0);
+			
+			setfillstyle(1, color[2]);
+			floodfill(from[starting[2]][0], from[starting[2]][1] + 40, 0);
+			rectangle(from[starting[1]][0] + 60, 400, from[starting[1]][0]+220, 560);
+			circle(440, 440, 25);circle(440, 520, 25);circle(520, 440, 25);circle(520, 520, 25);
+			setfillstyle(1, 15);
+			floodfill(from[starting[2]][0], from[starting[2]][1] + 80, 0);
+			
+			setfillstyle(1, color[3]);
+			floodfill(from[starting[3]][0] - 40, from[starting[3]][1], 0);
+			rectangle(from[starting[0]][0]-20, 400, from[starting[0]][0]+140, 560);
+			circle(80, 440, 25);circle(160, 440, 25);circle(80, 520, 25);circle(160, 520, 25);
+			setfillstyle(1, 15);
+			floodfill(from[starting[3]][0] - 80, from[starting[3]][1], 0);
+			
 			settextstyle(0, HORIZ_DIR, 2);
-			outtextxy(250, 300, "WINNER");
+			outtextxy(255, 290, "WINNER");
 		}
+		
 		int roll_dice(){
 			char dice[2], ch;
 			int num;
@@ -107,38 +141,37 @@ class chopad{
 class goti{
 	public:
 		static int occur;
-		int location[4][2], startx, starty, current, viewx, viewy, color;
-		char name;
+		int location[4][2], turning[2], startx, starty, current, color;
 		goti(){
 			current = -1;
 			switch(occur){
 				case 0:
-					startx = 52;
-					starty = 252;
-					viewx = startx;
-					viewy = starty - 40;
+					startx = 60;
+					starty = 260;
 					color = 4;
+					turning[0] = 20;
+					turning[1] = 300;
 				break;
 				case 1:
-					startx = 332;
-					starty = 52;
-					viewx = startx + 40;
-					viewy = starty;
+					startx = 340;
+					starty = 60;
 					color = 2;
+					turning[0] = 300;
+					turning[1] = 20;
 				break;
 				case 2:
-					startx = 532;
-					starty = 332;
-					viewx = startx;
-					viewy = starty + 40;
+					startx = 540;
+					starty = 340;
 					color = 14;
+					turning[0] = 580;
+					turning[1] = 300;
 				break;	
 				case 3:
-					startx = 252;
-					starty = 532;
-					viewx = startx - 40;
-					viewy = starty;
+					startx = 260;
+					starty = 540;
 					color = 1;
+					turning[0] = 300;
+					turning[1] = 580;
 				break;			
 			}
 			for(int i = 0;i < 4;i++){
@@ -148,8 +181,8 @@ class goti{
 			occur++;
 		}
 		
-		void move(int dice){
-			int num;
+		int move(int dice){
+			int num,  status = 0;
 			char ch[2];
 			ch[0] = '2';
 			ch[1] = '\0';
@@ -168,7 +201,8 @@ class goti{
 					current++;
 					location[current][0] = startx;
 					location[current][1] = starty;
-				}	
+					return 0;
+				}
 			}
 			if((location[0][0] != 0) && (ch[0] == '2')){
 				do{
@@ -177,61 +211,71 @@ class goti{
 					num = atoi(ch);
 					current = num - 1;
 				}while(location[current][0] == 0);
-				steps(dice);
+				status = steps(dice);
+				return status;
 			}
+			return 0;
 		}
 		
 		void goti_pos(){
 			settextstyle(0, HORIZ_DIR, 2);
 			for(int i = 0;i < 4;i++){
 				if(location[i][0] != 0){
-					circle(location[i][0] + 7, location[i][1] + 10 , 15);
+					circle(location[i][0], location[i][1], 15);
 					setfillstyle(SOLID_FILL, color);
 					floodfill(location[i][0], location[i][1], 0);
 				}
-				setfillstyle(SOLID_FILL, color);
-				floodfill(viewx, viewy, 0);
 			}
 		}
 		
-		void steps(int steps){
-			char remp[2];
-			remp[1] = '\0';
-			int i = 0;
+		int steps(int steps){
+			int i = 0, scolor, var = 0;
 			while(1){
 				if(location[current][0] == from[i][0]){
-					if(((location[current][1] >= from[i][1]) && (location[current][1] <= to[i][1])) || ((location[current][1] <= from[i][1]) && (location[current][1] >= to[i][1])))
+					if(((location[current][1] >= from[i][1]) && (location[current][1] <= from[i+1][1])) || ((location[current][1] <= from[i][1]) && (location[current][1] >= from[i+1][1])))
 							break;
 				}
 				else if(location[current][1] == from[i][1]){
-					if(((location[current][0] >= from[i][0]) && (location[current][0] <= to[i][0])) || ((location[current][0] <= from[i][0]) && (location[current][0] >= to[i][0])))
+					if(((location[current][0] >= from[i][0]) && (location[current][0] <= from[i+1][0])) || ((location[current][0] <= from[i][0]) && (location[current][0] >= from[i+1][0])))
 							break;
 				}
 				i++;
 			}
+			
 			while(steps-- > 0){
-				if((location[current][0] == to[i][0]) && (location[current][1] == to[i][1]))
+				if((location[current][0] == from[(i+1)%24][0]) && (location[current][1] == from[(i+1)%24][1]))
 					i = (++i)%24;
-				if(to[i][0] > location[current][0]){
+				if(from[(i+1)%24][0] > location[current][0]){
 					location[current][0] += 40;
 				}
-				else if(to[i][0] < location[current][0]){
+				else if(from[(i+1)%24][0] < location[current][0]){
 					location[current][0] -= 40;
 				}
 				
-				if(to[i][1] > location[current][1]){
+				if(from[(i+1)%24][1] > location[current][1]){
 					location[current][1] += 40;
 				}
-				else if(to[i][1] < location[current][1]){
+				else if(from[(i+1)%24][1] < location[current][1]){
 					location[current][1] -= 40;
 				}
 			}
+			scolor = getpixel(location[current][0], location[current][1]);
+			if((getpixel(location[current][0] - 18, location[current][1]) == 15) && (scolor != 15)){
+				while(1){
+					if(scolor == ::color[var])
+						break;
+					var++;
+				}
+				return var;
+			}
+			else 
+				return 0;
 		}
 }obj[4];
 int goti::occur = 0;
 
 int main(){
-	int dice;
+	int dice, status = 0, l;
 	char temp[2];
 	chopad table;
 	temp[1] = '\0';
@@ -249,8 +293,24 @@ int main(){
 			obj[j].goti_pos();
 		}
 		dice = table.roll_dice();
-		obj[k].move(dice);
-		if(dice == 6)
+		status = obj[k].move(dice);
+		if(status!=0){
+			for(int j = 0;j < 4;j++){
+				if((obj[k].location[obj[k].current][0] == obj[status].location[j][0]) && (obj[k].location[obj[k].current][1] == obj[status].location[j][1])){
+					l= j;
+					break;
+				}
+			}
+			for(int j = 0;j < 4;j++){
+				if(obj[status].location[j][0] == 0){
+					obj[status].location[l][0] = obj[status].location[j-1][0];
+					obj[status].location[l][1] = obj[status].location[j-1][1];
+					obj[status].location[j-1][0] = 0;
+					obj[status].location[j-1][1] = 0;
+				}
+			}
+		}
+		if(dice == 6 || status != 0)
 			k--;
 		k++;
 	}
